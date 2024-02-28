0fdb0509148efcbd475454ecce73636d98d976bf
Prawy_126 <michal190713@gmail.com>
2024-02-28 22:51:12 +0100

    Schemat hornera

---
+++ horner/main.py
@@ -0,0 +1,19 @@
+def horner(tablica, x):
+    wynik = tablica[0]
+    for i in range(1, len(tablica)):
+        wynik = wynik * x + tablica[i]
+    return wynik
+
+stopien_wielomianu = int(input("Podaj stopień wielomianu: "))
+tablica = []
+
+print("Podaj współczynniki wielomianu, zaczynając od współczynnika przy najwyższej potędze:")
+for i in range(stopien_wielomianu + 1):
+    y = int(input())
+    tablica.append(y)
+
+x = int(input("Podaj wartość x: "))
+
+print("Oto twoja tablica współczynników:", tablica)
+wynik = horner(tablica, x)
+print("Wynik wielomianu dla x =", x, "wynosi", wynik)
